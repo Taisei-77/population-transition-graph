691e24200b82b677f921e5ec4070dc7f5b90de0b
村上　 泰誠 <korokoro519@gmail.com>
2020-09-20 17:55:50 +0900

    都道府県にチェックを入れると、RESAS APIから、選択された都道府県の「総人口」を取得する

--- hoge/src/Prefectures.js
+++ hoge/src/Prefectures.js
@@ -13,6 +13,7 @@ const populationAPI =
 // APIの最後に ?prefCode=〇〇　とつけると、その都道府県の人口構成を取得することができる。
 
 const Prefectures = () => {
+  const [selected, setSelected] = useState(Array(47).fill(false));
   const [prefectures, setPrefectures] = useState([]);
 
   useEffect(() => {
@@ -21,7 +22,7 @@ const Prefectures = () => {
       "X-API-KEY": apiKey,
     };
     //RESASから47都道府県データを取得
-    const get_prefectures_data = async () => {
+    const getPrefecturesData = async () => {
       await axios
         .get(PrefecturesApi, { headers: headers })
         .then((res) => {
@@ -33,14 +34,53 @@ const Prefectures = () => {
           alert(error);
         });
     };
-    get_prefectures_data();
+    getPrefecturesData();
   }, []);
+
+  //都道府県チェックボックスの値が変わった際に実行される関数
+  const changeSelected = (indexNumber) => {
+    const selectedCopy = selected.slice();
+    //変更があった都道府県チェックボックスに対応している、select配列の値を反転させる。
+    selectedCopy[indexNumber] = !selectedCopy[indexNumber];
+    console.log(selectedCopy);
+
+    if (selectedCopy[indexNumber]) {
+      //RESASのAPIにアクセスするためのリクエストヘッダー
+      const headers = {
+        "X-API-KEY": apiKey,
+      };
+      //人口構成を取得
+      const getPopulation = async () => {
+        await axios
+          .get(
+            `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${
+              indexNumber + 1
+            }`,
+            { headers: headers }
+          )
+          .then((res) => {
+            console.log(res.data.result.data[0]); //対象都道府県の総人口データ。
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+      getPopulation();
+    }
+    setSelected(selectedCopy);
+  };
 
   //map()で各都道府県のデータを引数dataに渡して、要素を生成するための変数定義。
   const checkboxItem = (data) => {
     return (
       <div key={data.prefCode}>
-        <input type="checkbox" onChange></input>
+        <input
+          type="checkbox"
+          checked={selected[data.prefCode - 1]}
+          onChange={() => {
+            changeSelected(data.prefCode - 1);
+          }}
+        ></input>
         {data.prefName}
       </div>
     );
