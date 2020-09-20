2743f6d7d20be2c9d55514e4abaf8d71dfc4c927
村上　 泰誠 <korokoro519@gmail.com>
2020-09-20 14:34:48 +0900

    不要ファイルの削除

--- hoge/src/index.js
+++ hoge/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM, { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Test from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Foo = () => {
+  return (
+    <>
+      <Test />
+    </>
+  );
+};
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<Foo />, root);
