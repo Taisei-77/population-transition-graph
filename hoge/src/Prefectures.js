1d3225da7fd6efa1cbd9783c346254a36434d8d9
村上　 泰誠 <korokoro519@gmail.com>
2020-09-23 12:43:26 +0900

    人口構成APIレスポンスから、都道府県別の人口グラフを動的に生成するようにした。

--- hoge/src/Prefectures.js
+++ hoge/src/Prefectures.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 //RESASのAPIkey
 const apiKey = "0NEv0Ri2nZMNnApZ3qrtkDf0jCKuolBfrrIwx8d8";
@@ -8,14 +10,51 @@ const apiKey = "0NEv0Ri2nZMNnApZ3qrtkDf0jCKuolBfrrIwx8d8";
 const PrefecturesApi = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
 
 //RESASの人口構成データを取得するためのAPI
-const populationAPI =
-  "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
+//   "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear"
 // APIの最後に ?prefCode=〇〇　とつけると、その都道府県の人口構成を取得することができる。
 
 const Prefectures = () => {
-  const [selected, setSelected] = useState(Array(47).fill(false));
-  const [prefectures, setPrefectures] = useState([]);
+  const [selected, setSelected] = useState(Array(47).fill(false)); //チェックボックスにチェックが入っているかをtrueとfalseで判定する。
+  const [prefectures, setPrefectures] = useState([]); //都道府県データを格納する。
+  const [series, setSeries] = useState([]); //動的に取得した都道府県別の人口データを格納する。
+  //HighChartの設定
+  const options = {
+    title: {
+      text: "都道府県別の総人口推移",
+    },
 
+    yAxis: {
+      title: {
+        text: "総人口数",
+      },
+    },
+
+    xAxis: {
+      title: {
+        text: "年度",
+      },
+    },
+
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
+    },
+
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: 1960,
+        pointInterval: 5,
+      },
+    },
+
+    series: series,
+  };
+
+  //初回レンダリング時に実行される。
   useEffect(() => {
     //RESASのAPIにアクセスするためのリクエストヘッダー
     const headers = {
@@ -59,13 +98,35 @@ const Prefectures = () => {
             { headers: headers }
           )
           .then((res) => {
-            console.log(res.data.result.data[0]); //対象都道府県の総人口データ。
+            let populationData = []; //人口データを配列で保存するための変数
+            //対象都道府県の総人口データ => res.data.result.data[0].data
+            //レスポンスから人口データのみを抜き出して、populationDataに格納していく。
+            res.data.result.data[0].data.forEach((element) => {
+              populationData.push(element.value);
+            });
+            //seriesに追加するためのデータを作る。
+            const seriesData = {
+              name: prefectures[indexNumber].prefName,
+              data: populationData,
+            };
+            console.log([...series, seriesData]);
+            setSeries([...series, seriesData]);
           })
           .catch((error) => {
             alert(error);
           });
       };
       getPopulation();
+    } else {
+      //チェックが外された場合 =>　series配列から外された都道府県の人口データを取り除く。
+      const newSeries = [];
+      series.forEach((element) => {
+        if (element.name === prefectures[indexNumber].prefName) {
+        } else {
+          newSeries.push(element);
+        }
+      });
+      setSeries(newSeries);
     }
     setSelected(selectedCopy);
   };
@@ -89,6 +150,7 @@ const Prefectures = () => {
   return (
     <div>
       <div>{prefectures.map((data) => checkboxItem(data))}</div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 };
